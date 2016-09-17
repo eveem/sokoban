449d4e722f7a53dfae3ad1edd96879ae9d1fff15
eveem <monpriya.tammavong@gmail.com>
2016-09-18 02:46:02 +0700

    Can check boxes position

--- src/sokoban/GameBoardTest.java
+++ src/sokoban/GameBoardTest.java
@@ -82,5 +82,23 @@ public class GameBoardTest {
 	        assertFalse(smallBoard.hasExitAt(100, 1));
 	        assertFalse(smallBoard.hasExitAt(1, 500));
 	    }
+	    
+	    @Test
+	    public void testBoxPositionCheck() {
+	        assertFalse(smallBoard.hasBoxAt(0, 0));
+	    }
+	    
+	    @Test
+	    public void testBoxPositionCheckReturnTrue() {
+	        assertTrue(smallBoard.hasBoxAt(1, 2));
+	        assertTrue(smallBoard.hasBoxAt(2, 4));
+	    }
+	    
+	    @Test
+	    public void testBoxPositionCheckOutsideBoard() {
+	        assertFalse(smallBoard.hasBoxAt(-10, -1));
+	        assertFalse(smallBoard.hasBoxAt(100, 1));
+	        assertFalse(smallBoard.hasBoxAt(1, 500));
+	    }
 
 }
